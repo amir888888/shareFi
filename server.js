916831364a2320aea1c7ab96e27499af68f35eb2
Mohammed Amir Khan <khanmohammedamirkhan@gmail.com>
2022-02-14 20:53:29 +0530

    correcting port heroku

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 app.use(cors())
 app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
+
 // app.use(bodyParser.json({ limit: '50mb' }));
 // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 app.use(express.json({limit: '109mb'}));
 app.use(express.urlencoded({limit: "109mb", extended: true, parameterLimit:5000}));
 app.use(bodyParser.json({limit: '109mb'}));
-const PORT = process.env.PORT || 5000;
 
 const connectDb = require('./config/db');
 
@@ -27,9 +28,9 @@ app.set('view engine', 'ejs')
 
 
 //Routes
-app.use('/',fileRoutes);
+app.use('/api/files',fileRoutes);
 app.use('/files', downloadPage)
 app.use('/files/download', download);
-app.listen(PORT, () =>{
+app.listen(app.get(port), () =>{
     console.log(`Listening on port ${PORT}`);
 })
