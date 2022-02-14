05153d946cac01dd258bebe1a6db03574f1ce74f
Mohammed Amir Khan <khanmohammedamirkhan@gmail.com>
2022-02-14 22:30:05 +0530

    correcting port again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 app.use(cors())
 app.use(express.static('public'));
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
 // app.use(bodyParser.json({ limit: '50mb' }));
 // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
@@ -31,6 +31,6 @@ app.set('view engine', 'ejs')
 app.use('/api/files',fileRoutes);
 app.use('/files', downloadPage)
 app.use('/files/download', download);
-app.listen(app.get(port), () =>{
+app.listen(app.get(process.env.PORT || 5000), () =>{
     console.log(`Listening on port ${PORT}`);
 })
