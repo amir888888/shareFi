84bf91b02407c51c6cce2c61eb5cc20d1784c51c
Mohammed Amir Khan <khanmohammedamirkhan@gmail.com>
2022-02-14 20:28:23 +0530

    correcting url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 app.use(express.json({limit: '109mb'}));
-app.use(express.urlencoded({limit: "109mb", extended: true, parameterLimit:50000000000000000}));
+app.use(express.urlencoded({limit: "109mb", extended: true, parameterLimit:5000}));
 app.use(bodyParser.json({limit: '109mb'}));
 const PORT = process.env.PORT || 5000;
 
